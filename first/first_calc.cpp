4bbffb1316a0a0c4aaaabeaee07bf8783ca450cf
Liz <73101718+Stegogo@users.noreply.github.com>
2021-09-19 23:13:56 +0300

    1.0

--- first/first_calc.cpp
+++ first/first_calc.cpp
@@ -7,40 +7,23 @@ float first(float x, float X)
 	return((abs(x - X)) / X);
 }
 
-int main(int argc, char* argv[])
+int main(float n1=0, float x1=0, float n2=0, float n22=1, float x2=0)
 {
-	float n1 = 0;
-	cin >> n1;
-
-	float x1 = 0;
-	cin >> x1;
-
 	float dx1 = 0;
 	dx1 = first(x1, sqrt(n1));
-	
-	cout << "dx1="<<dx1 << " \n";
-
-	float n2 = 0;
-	cin >> n2;
-		float N2 = 0;
-	cin >> N2;
-
-	float x2 = 0;
-	cin >> x2;
 
 	float dx2 = 0;
-	dx2 = first(x2, (n2 / N2));
-
-	cout <<"dx2="<< dx2 << "\n";
+	dx2 = first(x2, (n2 / n22));
 	
 	float L_answer = 0;
 	if (dx1 < dx2)
 		L_answer = dx1;
 	else
 		L_answer = dx2;
+
 	if (L_answer == dx1)
-		cout << "sqrt(" << n1 << ")" << " is more accurate \n";
+		return 1;
 	else
-		cout << n2 << "/" << N2 << " is more accurate \n";
+		return 2;
 return 0;
 }
--- first/first_misc.py
+++ first/first_misc.py
@@ -1,6 +1,13 @@
+import dearpygui.dearpygui as dpg
+
+def calc(n1, x1, n2, n22, x2):
+    pass
+
 def save_callback():
     print("Clicked!")
-
-
-def calc():
-    pass
+    n1 = dpg.get_value("n1")
+    x1 = dpg.get_value("x1")
+    n2 = dpg.get_value("n2")
+    n22 = dpg.get_value("n22")
+    x2 = dpg.get_value("x2")
+    calc(n1, x1, n2, n22, x2)
