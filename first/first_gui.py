c8576b05daea7c5f9edfc3323af6314dda438d40
Liz <73101718+Stegogo@users.noreply.github.com>
2021-09-20 20:32:23 +0300

    1.1

--- first/first_gui.py
+++ first/first_gui.py
@@ -1,8 +1,6 @@
 import dearpygui.dearpygui as dpg
 from first import first_misc
 
- 
-
 def first1():
     dpg.add_text("Determine the most accurate equation:")
     dpg.add_text("1)    √")
@@ -25,11 +23,10 @@ def first1():
     dpg.add_input_text(width=100, id="x2", decimal=True)
 
     dpg.add_button(label="Go!", callback=first_misc.save_callback, id="go")
-    dpg.set_item_pos("go", [500, 130])
+    dpg.set_item_pos("go", [500, 170])
 
 def first2():
     dpg.add_text("Round the doubtful digits and \nleave the correct digits of the number:")
-    #dpg.add_radio_button("Radio1", items=["itemA", "itemB"], callback=first_misc.secondRadio)
     dpg.add_input_text(width=100, id="a1", decimal=True)
     dpg.add_same_line(spacing=10)
     dpg.add_text("(±")
@@ -38,10 +35,19 @@ def first2():
     dpg.add_same_line(spacing=10)
     dpg.add_text(")")
     dpg.add_button(label="Go!", callback=first_misc.save_callback, id="go2")
-    dpg.set_item_pos("go2", [500, 150])
+    dpg.set_item_pos("go2", [500, 165])
+    dpg.add_input_text(width=100, id="b1", decimal=True)
+    dpg.add_same_line(spacing=10)
+    dpg.add_text("(δ")
+    dpg.add_same_line(spacing=10)
+    dpg.add_input_text(width=100, id="B1", decimal=True)
+    dpg.add_same_line(spacing=10)
+    dpg.add_text(")")
+    dpg.add_button(label="Go!", callback=first_misc.save_callback, id="go22")
+    dpg.set_item_pos("go22", [500, 225])
 
 def first3():
     dpg.add_text("Find the marginal absolute and relative\nerrors of numbers that have\nonly correct digits:")
     dpg.add_input_text(width=400, id="c1", decimal=True)
     dpg.add_button(label="Go!", callback=first_misc.save_callback, id="go3")
-    dpg.set_item_pos("go3", [500, 180])
+    dpg.set_item_pos("go3", [500, 200])
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ comfortaa_path = os.environ['COMFORTAA_FONT_PATH']
 with dpg.font_registry():
     dpg.add_font(comfortaa_path, 35)
     with dpg.font(comfortaa_path, 35, default_font=True):
-        dpg.add_font_chars([0x221A, 0x8723])
+        dpg.add_font_chars([0x221A, 0x8723, 0x03B4])
 
 
 with dpg.window(label="Numbering Methods", id="main_window"):
@@ -24,13 +24,13 @@ with dpg.window(label="Numbering Methods", id="main_window"):
 
 dpg.setup_viewport()
 dpg.set_viewport_width(700)
-dpg.set_viewport_height(300)
+dpg.set_viewport_height(350)
 dpg.set_primary_window("main_window", True)
 
 with dpg.theme(default_theme=True):
     dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
     dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 20, 20, category=dpg.mvThemeCat_Core)
-    dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 10, 10, category=dpg.mvThemeCat_Core)
+    dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 10, 20, category=dpg.mvThemeCat_Core)
     dpg.add_theme_color(dpg.mvThemeCol_Button, [255, 117, 23], category=dpg.mvThemeCat_Core)
     dpg.add_theme_color(dpg.mvThemeCol_ButtonActive, [255, 222, 31], category=dpg.mvThemeCat_Core)
     dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered, [255, 164, 0], category=dpg.mvThemeCat_Core)
@@ -41,6 +41,7 @@ with dpg.theme(id="go_button"):
 
 dpg.set_item_theme("go", "go_button")
 dpg.set_item_theme("go2", "go_button")
+dpg.set_item_theme("go22", "go_button")
 dpg.set_item_theme("go3", "go_button")
 
 dpg.start_dearpygui()
