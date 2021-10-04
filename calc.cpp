14b684bba48ff78433f0d5ee9d52d64cfa4225b2
stegogo <lizmahrk@gmail.com>
2021-10-04 21:31:39 +0300

    2.0

--- calc.cpp
+++ calc.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iomanip>
 #include <iostream>
 using namespace std;
@@ -7,7 +8,22 @@ float first(float x, float X)
 	return((abs(x - X)) / X);
 }
 
-int calc(float x1=0, float x2=0, float x3=0, float x4=0,
+int calc1(float n1=0, float x1=0, float n2=0, float n22=1, float x2=0)
+{
+	float dx1 = 0;
+	dx1 = first(x1, sqrt(n1));
+
+	float dx2 = 0;
+	dx2 = first(x2, (n2 / n22));
+	
+	if (dx1 < dx2)
+		return 1;
+	else
+		return 2;
+	return 0;
+}
+
+int calc2(float x1=0, float x2=0, float x3=0, float x4=0,
 		float fx1=1, float fx2=1, float fx3=1, float fx4=1)
 {
 	double arrX[] = {x1, x2, x3, x4};
@@ -42,11 +58,21 @@ int calc(float x1=0, float x2=0, float x3=0, float x4=0,
 	return 0;
 }
 
+
 extern "C" {
-    int cppcalc2(float x1=0, float x2=0, float x3=0, float x4=0,
+    int cppcalc1(float n1=0, float x1=0, float n2=0, float n22=1, float x2=0)
+    {
+        if (calc1(n1, x1, n2, n22, x2) == 1)
+			return 1;
+		else
+			return 2;
+		return 0;
+    }
+
+	int cppcalc2(float x1=0, float x2=0, float x3=0, float x4=0,
 				float fx1=1, float fx2=1, float fx3=1, float fx4=1)
     {
-        if (calc(x1, x2, x3, x4, fx1, fx2, fx3, fx4) == 1)
+        if (calc2(x1, x2, x3, x4, fx1, fx2, fx3, fx4) == 1)
 			return 1;
 		else
 			return 2;
