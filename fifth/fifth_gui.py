32277d0f0b7340ac5f6e3afa4c080a5bc8fe7c35
stegogo <lizmahrk@gmail.com>
2021-11-16 02:13:32 +0200

    5.0

--- fifth/fifth_gui.py
+++ fifth/fifth_gui.py
@@ -2,10 +2,19 @@ import dearpygui.dearpygui as dpg
 from fifth import fifth_misc
 
 def fifth1():
-    dpg.add_text("Solve the nonlinear equation and\nspecify the roots with Newton\nand bisection methods.")
+    dpg.add_text("Solve the nonlinear equation.\nSpecify the roots with Newton\nand bisection methods.")
     dpg.add_input_text(width=450, id="5expr")
     dpg.add_same_line(spacing=10)
+    dpg.add_text("[")
+    dpg.add_same_line(spacing=10)
+    dpg.add_input_text(width=50, id="5_l_limit")
+    dpg.add_same_line(spacing=10)
+    dpg.add_text(";")
+    dpg.add_same_line(spacing=10)
+    dpg.add_input_text(width=50, id="5_r_limit")
+    dpg.add_same_line(spacing=10)
+    dpg.add_text("]")
     dpg.add_button(label="Go!", callback=fifth_misc.save_callback, id="go5")
-    dpg.set_item_pos("go5", [500, 195])
+    dpg.set_item_pos("go5", [540, 105])
     
     
--- fifth/fifth_misc.py
+++ fifth/fifth_misc.py
@@ -9,28 +9,43 @@ got_res = False
 handle = ctypes.CDLL("./libtest.so")     
 handle.cppcalc1.argtypes = [ctypes.c_float]
 
-def calc(n1, x1, n2, n22, x2):
-    res = handle.cppcalc1(ctypes.c_float(n1), ctypes.c_float(x1), ctypes.c_float(n2), ctypes.c_float(n22), ctypes.c_float(x2))
-    if res == 1:
-        return f"√{int(n1)} is more accurate."
-    elif res == 2:
-        return f"{int(n2)}/{int(n22)} is more accurate."
-    else:
-        return "Calculation error."
+def newton(r_limit, l_limit, num_expr, diff_expr):
+    arr_newton = []
+    x = sym.symbols('x')
+    for i in np.arange(r_limit, l_limit, 0.01):
+        y1 = num_expr.subs(x, i)
+        y2 = num_expr.subs(x, i+0.01)
+        if (y1>0 and y2<0) or (y1<0 and y2>0):
+            q = fabs(i-(i-num_expr.subs(x, i)/diff_expr.subs(x, i)))
+            while (q>0.0001):
+                i+=0.01
+                x1=i
+                x2=x1-num_expr.subs(x, x1)/diff_expr.subs(x, x1)
+                x1=x2
+                q=fabs(x1-x2)
+            arr_newton.append(round(x1, 5))
+    return arr_newton
+              
 
 def save_callback():
     global got_res
-
-    expr = dpg.get_value("5expr")
     x = sym.symbols('x')
+    expr = dpg.get_value("5expr")
+    
     num_expr =  sym.parse_expr(expr)
-    a = num_expr.subs(x, 1)
+    diff_expr = sym.diff(num_expr, x)
     
+    l_limit = float(dpg.get_value("5_l_limit"))
+    r_limit = float(dpg.get_value("5_r_limit"))
     
+    arr_newton = newton(l_limit, r_limit, num_expr, diff_expr)
 
     if got_res == False:
-        dpg.add_text(f"a", before="5expr", id="5_result")
+        dpg.add_text(f"Newton method:\n{arr_newton}", before="5expr", id="5_result_newton")
+        dpg.add_text(f"Bisection method:\n{arr_newton}", before="5_result_newton", id="5_result_bisection")
         got_res = True
     else:
-        dpg.delete_item("5_result")
-        dpg.add_text(f"a", before="5expr", id="5_result")
+        dpg.delete_item("5_result_newton")
+        dpg.delete_item("5_result_bisection")
+        dpg.add_text(f"Newton method: {arr_newton}", before="5expr", id="5_result_newton")
+        dpg.add_text(f"Bisection method: {arr_newton}", before="5_result_newton", id="5_result_bisection")
