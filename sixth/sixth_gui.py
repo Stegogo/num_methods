9a71e2321fd19e89d5ab91afb681bff35e2572f9
stegogo <lizmahrk@gmail.com>
2021-12-07 01:06:36 +0200

    6.0

--- sixth/sixth_gui.py
+++ sixth/sixth_gui.py
@@ -2,7 +2,10 @@ import dearpygui.dearpygui as dpg
 from sixth import sixth_misc
 
 def sixth1():
-    
-
+    dpg.add_text("Compose a solution of the Cauchy problem\n"
+                 "for the ordinary first-order differential\n"
+                 "equation on a segment with a step h=0.1\n"
+                 "under the inital conditions:\n")
+    dpg.add_radio_button(label="Radio A", items=["Euler method", "Euler-Cauchy method"], callback=sixth_misc.save_callback)
     dpg.add_button(label="Go!", callback=sixth_misc.save_callback, id="go6")
     dpg.set_item_pos("go6", [500, 170])
